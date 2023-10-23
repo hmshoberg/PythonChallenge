df8048342295f315f6ea0da90916f1a950be762a
hmshoberg <145800386+hmshoberg@users.noreply.github.com>
2023-10-23 13:47:54 -0500

    Final except average is off

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,7 +8,10 @@
 import os
 import csv
 
-budget_csv = os.path.join("Resources", "budget_data.csv")
+budget_csv = os.path.join("..", "Resources","budget_data.csv")
 
 with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+
+
+
